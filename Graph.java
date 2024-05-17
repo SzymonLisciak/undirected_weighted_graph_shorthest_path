ba86eee6cd5fadea0f792248cb721f2f67265325
SzymonLisciak <155400528+SzymonLisciak@users.noreply.github.com>
2024-05-17 09:16:45 +0200

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,14 +1,17 @@
 import java.lang.reflect.Array;
 import java.util.*;
-public class Graph {
-    List<Node> nodes;
-    List<Edge> edges;
+import java.util.stream.Collectors;
 
-    public Graph(){
-        this.nodes = new ArrayList<>();
-        this.edges = new ArrayList<>();
+public class Graph {
+    Node[] nodes;
+    Edge[] edges;
+
+    public Graph() {
+        this.nodes = new Node[0];
+        this.edges = new Edge[0];
     }
-    public void addEdge(int id1, int id2, int weight){
+
+    public void addEdge(int id1, int id2, int weight) {
         Node v1 = findNode(id1);
         Node v2 = findNode(id2);
         for (Edge edge : edges) {
@@ -19,156 +22,136 @@ public class Graph {
         }
 
         if (v1 != null && v2 != null) {
-            edges.add(new Edge(v1, v2, weight));
-            System.out.println("Dodano nową Krawędź o wierzchołkach (które mają id) v1: " + v1.id + " " + "v2: " + v2.id + " " + "weight: " + weight);
+            edges = Arrays.copyOf(edges, edges.length + 1);
+            edges[edges.length - 1] = new Edge(v1, v2, weight);
+            System.out.println("Dodano nową Krawędź o wierzchołkach (które mają id) v1: " + v1.id + " v2: " + v2.id + " weight: " + weight);
         } else {
             System.out.println("Nie ma Node, który połączyłby się z tą krawędzią");
         }
     }
 
-
     public Edge findEdge(int id1, int id2) {
-        if (this.edges != null) {
-            for (int i = 0; i < this.edges.size(); i++) {
-                if (id1 == this.edges.get(i).v1.id && id2 == this.edges.get(i).v2.id) {
-                    return edges.get(i);
-                }
+        for (Edge edge : edges) {
+            if ((edge.v1.id == id1 && edge.v2.id == id2) || (edge.v1.id == id2 && edge.v2.id == id1)) {
+                return edge;
             }
-
         }
         return null;
     }
-    public List<Edge> getEdgeConnection(int id){
-        List<Edge> edgesTemp = new ArrayList<Edge>();
 
-        if (edges != null){
-            int i = 0;
-            for (i = 0; i < edges.size(); i++){
-                if (id == edges.get(i).v1.id || id == edges.get(i).v2.id){
-                    edgesTemp.add(edges.get(i));
-                }
+    public Edge[] getEdgeConnection(int id) {
+        Edge[] edgesTemp = new Edge[0];
+        for (Edge edge : edges) {
+            if (edge.v1.id == id || edge.v2.id == id) {
+                edgesTemp = Arrays.copyOf(edgesTemp, edgesTemp.length + 1);
+                edgesTemp[edgesTemp.length - 1] = edge;
             }
         }
-        else{
-            return null;
-        }
-
         return edgesTemp;
     }
 
     public void removeNodes(int id) {
         Node node = findNode(id);
         if (node != null) {
-            List<Edge> edgesRemoval = getEdgeConnection(id);
-            Edge edgeLog;
-                if (edgesRemoval != null) {
-                    for (int i = 0; i < edgesRemoval.size(); i++) {
-                        System.out.println("Usunięto krawędź z wagą" + edgesRemoval.get(i).weight + " i id1: " + edgesRemoval.get(i).v1.id + "oraz id2: " + edgesRemoval.get(i).v2.id);
-                        edges.remove(edgesRemoval.get(i));
-
-                    }
-                }
-                nodes.remove(node);
-                System.out.println("usunięto wierzchołek z id: " + id);
+            Edge[] edgesRemoval = getEdgeConnection(id);
+            for (Edge edge : edgesRemoval) {
+                System.out.println("Usunięto krawędź z wagą " + edge.weight + " i id1: " + edge.v1.id + " oraz id2: " + edge.v2.id);
+                edges = Arrays.stream(edges).filter(e -> e != edge).toArray(Edge[]::new);
+            }
+            nodes = Arrays.stream(nodes).filter(n -> n.id != id).toArray(Node[]::new);
+            System.out.println("usunięto wierzchołek z id: " + id);
+        } else {
+            System.out.println("Wierzchołek z id: " + id + " nie istnieje");
         }
-        else {
-            System.out.println("Wierzchołek z id: " + id + " " + "nie istnieje");
-        }
-
     }
 
     public void removeEdge(int id1, int id2) {
-        if (findEdge(id1, id2) != null) {
-            edges.remove(findEdge(id1, id2));
-            System.out.println("Usunięto krawędź przyczepionej do wierzchołków o id1 :" + id1 + " " + "id2: " + id2);
-        }
-        else{
-            System.out.println("Nie znaleziono krawędzi o wierzchołkah o id1 :" + id1 + " " + "id2: " + id2);
+        Edge edge = findEdge(id1, id2);
+        if (edge != null) {
+            edges = Arrays.stream(edges).filter(e -> e != edge).toArray(Edge[]::new);
+            System.out.println("Usunięto krawędź przyczepionej do wierzchołków o id1: " + id1 + " id2: " + id2);
+        } else {
+            System.out.println("Nie znaleziono krawędzi o wierzchołkach o id1: " + id1 + " id2: " + id2);
         }
     }
 
     public void addNode(int id) {
         if (findNode(id) == null) {
-            this.nodes.add(new Node(id));
-            System.out.println("Dodano nowy NODE z id:" + id);
-        }
-        else {
-            System.out.println("Node z id: " + id + " " + "Już istnieje");
+            nodes = Arrays.copyOf(nodes, nodes.length + 1);
+            nodes[nodes.length - 1] = new Node(id);
+            System.out.println("Dodano nowy NODE z id: " + id);
+        } else {
+            System.out.println("Node z id: " + id + " już istnieje");
         }
     }
 
     public Node findNode(int id) {
-        for (int i = 0; i < this.nodes.size(); i++) {
-            if(this.nodes.get(i).id == id) {
-                return nodes.get(i);
+        for (Node node : nodes) {
+            if (node.id == id) {
+                return node;
             }
         }
         return null;
     }
-    public void djikstra(int sourceId) {
-    Node source = findNode(sourceId);
-    if (source == null) {
-        System.out.println("Nie istnieje wierzchołek o id: " + sourceId);
-        return;
-    }
 
+    public int dijkstra(int sourceId, int destinationId) {
+        Node source = findNode(sourceId);
+        Node destination = findNode(sourceId);
 
-    Map<Node, Integer> distance = new HashMap<>();
-    Map<Node, Node> previous = new HashMap<>(); 
-    for (Node node : nodes) {
-        distance.put(node, Integer.MAX_VALUE);
-        previous.put(node, null);
-    }
-    distance.put(source, 0);
-
-    Set<Node> visited = new HashSet<>();
-    while (!visited.containsAll(nodes)) {
-
-        int minDistance = Integer.MAX_VALUE;
-        Node u = null;
-        for (Node node : distance.keySet()) {
-            if (!visited.contains(node) && distance.get(node) < minDistance) {
-                u = node;
-                minDistance = distance.get(node);
-            }
+        if (source == null) {
+            System.out.println("Nie istnieje wierzchołek który ma być źródłem o id: " + sourceId);
+            return -1;
         }
-        if (u == null) {
-
-            break;
+        if (destination == null) {
+            System.out.println("Nie istnieje wierzchołek który ma być celem o id: " + sourceId);
+            return -1;
         }
 
-        visited.add(u);
+        int[] distance = new int[nodes.length];
+        boolean[] visited = new boolean[nodes.length];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[getIndex(source)] = 0;
 
- 
-        for (Edge edge : getEdgeConnection(u.id)) {
-            Node v = (edge.v1 == u) ? edge.v2 : edge.v1;
-            int alt = distance.get(u) + edge.weight;
-            if (alt < distance.get(v)) {
-                distance.put(v, alt);
-                previous.put(v, u); // Update previous node for v
+        while (true) {
+            int minDistance = Integer.MAX_VALUE;
+            Node u = null;
+            int uIndex = -1;
+            for (int i = 0; i < nodes.length; i++) {
+                if (!visited[i] && distance[i] < minDistance) {
+                    minDistance = distance[i];
+                    u = nodes[i];
+                    uIndex = i;
+                }
+            }
+            if (u == null) {
+                break;
+            }
+            if (u.id == destinationId) {
+                return distance[uIndex];
+            }
+
+            visited[uIndex] = true;
+
+            Edge[] edges = getEdgeConnection(u.id);
+            for (Edge edge : edges) {
+                Node v = (edge.v1 == u) ? edge.v2 : edge.v1;
+                int vIndex = getIndex(v);
+                int alt = distance[uIndex] + edge.weight;
+                if (alt < distance[vIndex]) {
+                    distance[vIndex] = alt;
+                }
             }
         }
+
+        return distance[getIndex(destination)];
     }
 
-
-    for (Node node : nodes) {
-        if (node != source) {
-            List<Node> path = new ArrayList<>();
-            Node current = node;
-            while (current != null) {
-                path.add(current);
-                current = previous.get(current);
-            }
-            Collections.reverse(path);
-            System.out.print("Shortest path from " + sourceId + " to " + node.id + ": ");
-            if (distance.get(node) < Integer.MAX_VALUE) {
-                System.out.print(path.stream().map(n -> String.valueOf(n.id)).collect(Collectors.joining(" -> ")));
-                System.out.println(" (Distance: " + distance.get(node) + ")");
-            } else {
-                System.out.println("unreachable");
+    private int getIndex(Node node) {
+        for (int i = 0; i < nodes.length; i++) {
+            if (nodes[i] == node) {
+                return i;
             }
         }
+        return -1;
     }
-}
-
 }
